1b6c3169e807dcde98aed807c15ca4dd8312c843
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-02-23 23:40:34 +0300

    GettingData projesi eklendi. Startup.cs'e eklenen endpoints.MapControllerRoute metodu ile endpoint üzerinden veri alma işlemleri sağlandı.

---
+++ GettingData/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using GettingData.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GettingData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(Product product)
+        {
+            return "Uygulama Çalışıyor";
+        }
+    }
+}
---
+++ GettingData/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace GettingData.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+    }
+}
