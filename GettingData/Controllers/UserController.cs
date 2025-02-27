daec83d7e1773a5bee0cbc3c6009116dd65207ed
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-02-28 00:54:56 +0300

    Form üzerinden veri alma örneği eklendi.

---
+++ GettingData/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using GettingData.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GettingData.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult AddUser(string FirstName, string LastName)
+        //{
+        //    return View();
+        //}
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            return View();
+        }
+    }
+}
