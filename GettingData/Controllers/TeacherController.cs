0ea08444cd652d8d7ce606837c0fcf17858fdafa
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-03-05 23:04:31 +0300

    Tuple nesnesi ile form üzerinden veri alma işlemleri eklendi.

--- GettingData/Controllers/TeacherController.cs
+++ GettingData/Controllers/TeacherController.cs
@@ -23,5 +23,16 @@ namespace GettingData.Controllers
             ViewBag.Branches = branches;
             return View();
         }
+        public IActionResult AddTeacherUsingTuple()
+        {
+            (List<Branch>, Teacher) tuple = (branches, new Teacher());
+            return View(tuple);//Tuple ile birden fazla veri view gönderilerek kullanılabilir.
+        }
+        [HttpPost]
+        public IActionResult AddTeacherUsingTuple([Bind(Prefix ="Item2")] Teacher teacher)
+        {
+            //Gönderilen bu verileri karşılamak için yukarıdaki şekilde kullanılır. Item2 olmasının sebebi yukarıda doldurduğumuz tuple'da ilk Itemda branches yer alıyor.
+            return RedirectToAction("AddTeacherUsingTuple");
+        }
     }
 }
