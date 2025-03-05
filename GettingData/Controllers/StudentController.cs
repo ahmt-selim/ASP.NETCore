0bdd5993ad23c1eaeed96130a70a62c535118a35
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-03-05 22:21:08 +0300

    form üzerinden veri alma örneği olarak basit öğrenci ekleme sayfası yapıldı.

---
+++ GettingData/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using GettingData.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GettingData.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student(){FirstName = "Ahmet", LastName= "Kısa"},
+            new Student(){FirstName = "Selim", LastName= "Akca"},
+            new Student(){FirstName = "Furkan", LastName= "Say"},
+            new Student(){FirstName = "Tolga", LastName= "Keskin"},
+            new Student(){FirstName = "İbrahim", LastName= "Yılmaz"}
+        };
+        public IActionResult GetStudentList()
+        {
+            return View(students);
+        }
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            students.Add(student);
+            return View("GetStudentList",students);
+        }
+    }
+}
---
+++ GettingData/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace GettingData.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
