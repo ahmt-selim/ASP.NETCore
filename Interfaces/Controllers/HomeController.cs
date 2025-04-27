74b2c95e16bfcf2b8f20390a326573a8648a6c26
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-04-27 23:10:33 +0300

    Interfaces projesi eklendi. Interface örnekleri yapıldı.

---
+++ Interfaces/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Interfaces.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Interfaces.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            MsSqlOperations mssql =new MsSqlOperations();
+            mssql.Connection();
+
+            ISqlOperations sql = new MsSqlOperations();
+            sql.Connection();
+
+            ISqlOperations mysql = new MySqlOperation();
+            mysql.Connection();
+
+            return View();
+        }
+    }
+}
---
+++ Interfaces/Models/MsSqlOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Interfaces.Models
+{
+    public class MsSqlOperations : Test, ISqlOperations, ITestInterface
+    {
+        public void Connection()
+        {
+            Console.WriteLine("MsSql için bağlantı sağlandı.");
+        }
+
+        public bool Insert(Products product)
+        {
+            return true;
+        }
+    }
+    public class MySqlOperation : ISqlOperations
+    {
+        public void Connection()
+        {
+            Console.WriteLine("Mysql bağlantısı sağlandı.");
+        }
+
+        public bool Insert(Products product)
+        {
+            return true;
+        }
+    }
+    public interface ISqlOperations
+    {
+        void Connection();
+        bool Insert(Products product);
+    }
+    public interface ITestInterface
+    {
+
+    }
+    public class Test
+    {
+
+    }
+}
