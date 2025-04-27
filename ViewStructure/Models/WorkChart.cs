97885905722d39fa3a66409d670cd6d87d703346
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-04-27 16:01:19 +0300

    ViewComponent kullanılarak öğretmenler listesine çalışma günleri eklendi.

---
+++ ViewStructure/Models/WorkChart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ViewStructure.Models
+{
+    public class WorkChart
+    {
+        public WorkChart(string _teacherName, List<bool> _chart) 
+        {
+            TeacherName = _teacherName;
+            Chart = _chart;
+        }
+        public string TeacherName { get; set; }
+        public List<bool> Chart { get; set; }
+    }
+}
---
+++ ViewStructure/ViewComponents/WorkChartViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ViewStructure.Models;
+
+namespace ViewStructure.ViewComponents
+{
+    public class WorkChartViewComponent : ViewComponent
+    {
+
+        List<WorkChart> workChart = new List<WorkChart>()
+        {
+            new WorkChart("Ahmet", new List<bool>{true, false, true, true, false}),
+            new WorkChart("Selim", new List<bool>{false, false, true, true, true}),
+            new WorkChart("Ayşe", new List<bool>{true, true, false, true, false}),
+            new WorkChart("Merve", new List<bool>{true, true, true, false, false})
+        };
+        public IViewComponentResult Invoke(int id)
+        {
+            return View(workChart);
+        }
+
+    }
+}
